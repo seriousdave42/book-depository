13562021ac058a3e8265211a80401a2e4beb3206
seriousdave42 <seriousdave42@gmail.com>
2020-08-06 20:32:45 -0500

    added repo and service

---
+++ src/main/java/com/dwatkins/mvc/services/BookService.java
@@ -0,0 +1,36 @@
+package com.dwatkins.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.dwatkins.mvc.models.Book;
+import com.dwatkins.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+	
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+	
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if(optionalBook.isPresent()) {
+			return optionalBook.get();
+		}
+		else {
+			return null;
+		}
+	}
+}
